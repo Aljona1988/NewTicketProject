4f66ec0999df060e29ecc374e6b9811fe5d3dbdb
Elena Fedkina <zhrulick@maiil.ru>
2022-10-25 23:51:30 +0300

    new classes

---
+++ src/main/java/Ticket.java
@@ -0,0 +1,49 @@
+public class Ticket implements Comparable<Ticket> {
+    protected int id;
+    protected int price;
+    protected String from;
+    protected String to;
+    protected int time;
+
+
+    public Ticket(int id, int price, String from, String to, int time) {
+        this.id = id;
+        this.price = price;
+        this.from = from;
+        this.to = to;
+        this.time = time;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+
+    @Override
+    public int compareTo(Ticket o) {
+        if (this.price < o.price) {
+            return -1;
+        } else if (this.price > o.price) {
+            return 1;
+        }
+        return 0;
+    }
+}
+
---
+++ src/test/java/TicketTest.java
@@ -0,0 +1,115 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TicketTest {
+    Ticket ticket1 = new Ticket(
+            12,
+            300,
+            "LED",
+            "GOJ",
+            90
+
+    );
+    Ticket ticket2 = new Ticket(
+            11,
+            200,
+            "MOW",
+            "GOJ",
+            60
+    );
+    Ticket ticket3 = new Ticket(
+            13,
+            250,
+            "GOJ",
+            "LED",
+            90
+    );
+    Ticket ticket4 = new Ticket(
+            14,
+            180,
+            "MOW",
+            "GOJ",
+            120
+    );
+    Ticket ticket5 = new Ticket(
+            15,
+            160,
+            "FRU",
+            "GOJ",
+            140
+    );
+    Ticket ticket6 = new Ticket(
+            16,
+            170,
+            "MOW",
+            "GOJ",
+            140
+    );
+    Ticket ticket7 = new Ticket(
+            17,
+            190,
+            "MOW",
+            "GOJ",
+            140
+    );
+
+    @Test
+    public void shouldFindDefTicket() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+
+        Ticket[] actual = manager.findAll("MOW", "GOJ");
+        Ticket[] expected = {ticket6, ticket4, ticket7, ticket2};
+
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldNotFind() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+
+        Ticket[] actual = manager.findAll("LED", "FRU");
+        Ticket[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void shouldDeleteById() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        repo.deleteById(ticket7.getId());
+
+        Ticket[] actual = repo.findAll();
+        Ticket[] expected = {ticket1, ticket2, ticket3, ticket4, ticket5, ticket6};
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
